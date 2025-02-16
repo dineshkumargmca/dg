eb7015b24bfc41ddfc290567078ab7a8bc3f9cd7
Dineshkumar Gajendiran <dineshkumarg.mca@gmail.com>
2025-02-16 10:15:22 +0800

    feat: angular library for reusable angular components

---
+++ libs/ngcomponents/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ngcomponents/ngcomponents.component';
---
+++ libs/ngcomponents/src/lib/ngcomponents/ngcomponents.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ngcomponents',
+  imports: [CommonModule],
+  templateUrl: './ngcomponents.component.html',
+  styleUrl: './ngcomponents.component.css',
+})
+export class NgcomponentsComponent {}
